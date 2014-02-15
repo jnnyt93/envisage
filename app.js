1c77d0bff3f91c42bd631107b5259745bc0dbe11
vincentdo <dodviet@sas.upenn.edu>
2014-02-15 10:22:19 -0500

    Add app.js

---
+++ app.js
@@ -0,0 +1,31 @@
+/* Some initialization boilerplate. Also, we include the code from
+   routes/routes.js, so we can have access to the routes. Note that
+   we get back the object that is defined at the end of routes.js,
+   and that we use the fields of that object (e.g., routes.get_main)
+   to access the routes. */
+
+var express = require('express');
+var routes = require('./routes/routes.js');
+var path = require('path');
+var app = express();
+
+app.use(express.bodyParser());
+app.use(express.logger("default"));
+app.use(express.cookieParser());
+app.use('/css', express.static(path.join(__dirname, 'css')));
+app.use('/js', express.static(path.join(__dirname, 'js')));
+// Using random 20 digit prime as secret key
+app.use(express.session({secret:'48112959837082048697'}));
+
+/* Below we install the routes. The first argument is the URL that we
+   are routing, and the second argument is the handler function that
+   should be invoked when someone opens that URL. Note the difference
+   between app.get and app.post; normal web requests are GETs, but
+   POST is often used when submitting web forms ('method="post"'). */
+
+app.get('/', routes.get_main);
+
+
+/* Run the server */
+app.listen(3000);
+console.log('Server running on port 3000. Now open http://localhost:8080/ in your browser!');
